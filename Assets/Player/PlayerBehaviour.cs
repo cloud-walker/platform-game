2c43458a3c4bb5ebc669db0962a55598550d0de1
Luca Barone <baro.luc@gmail.com>
2016-11-15 21:25:26 +0100

    :sparkles: player init: he can move!

---
+++ Assets/Player/PlayerBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+	Rigidbody2D rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+  void FixedUpdate() {
+    float x = Input.GetAxis("Horizontal");
+    float y = Input.GetAxis("Vertical");
+
+    Vector2 movement = new Vector2(x, y);
+
+    rb.AddForce(movement * 100);
+  }
+}
