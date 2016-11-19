3faff6eb4d12b45286709c4b8714b86081167550
Luca Barone <baro.luc@gmail.com>
2016-11-19 21:23:05 +0100

    first health bar implementation

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+	Rect rect;
+	Texture2D texture;
+	float barWidth;
+	float barHeight = 10f;
+	float barX;
+	float barY = Screen.height / 12;
+	float maxHealth = 100f;
+
+	[Range(0, 100)]
+	public float currentHealth;
+
+	void Start() {
+		currentHealth = maxHealth;
+		barWidth = maxHealth;
+		barX = Screen.width - barWidth - (barWidth / 3);
+		barY = barHeight + (barWidth / 5);
+		rect = new Rect(barX, barY, barWidth, barHeight);
+		texture = new Texture2D(1, 1);
+		texture.SetPixel(0, 0, Color.red);
+		texture.Apply();
+	}
+
+	void OnGUI() {
+		float ratio = currentHealth / maxHealth;
+		rect.width = ratio * barWidth;
+		GUI.DrawTexture(rect, texture);
+	}
+}
